78879658f2734e4229f1e39ccabf1e64abaa9f5d
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-28 19:45:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
 stack = []
+def PushZero():
+  stack.append(0)
+def PushOne():
+  stack.append(1)
+def PushTwo():
+  stack.append(2)
+def PushThree():
+  stack.append(3)
+def PushFour():
+  stack.append(4)
+def PushFive():
+  stack.append(5)
+def PushSix():
+  stack.append(6)
+def PushSeven():
+  stack.append(7)
+def PushEight():
+  stack.append(8)
+def PushNine():
+  stack.append(9)
 def Add():
-  u=stack.pop()
   v=stack.pop()
+  u=stack.pop()
   stack.append(u+v)
 def Subtract():
-  u=stack.pop()
   v=stack.pop()
-  stack.append(v-u)
+  u=stack.pop()
+  stack.append(u-v)
+def Multiply():
+  v=stack.pop()
+  u=stack.pop()
+  stack.append(u*v)
+def Divide():
+  v=stack.pop()
+  u=stack.pop()
+  while v == 0:
+    v = AskInteger()
+  stack.append(int(u/v))
+def Modulo():
+  v=stack.pop()
+  u=stack.pop()
+  while v == 0:
+    v = AskInteger()
+  stack.append(u%v)
+def Not():
+  u=stack.pop()
+  stack.append(int(not u))
