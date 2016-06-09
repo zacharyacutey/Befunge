3cc98c29b66bdb8a6cfbd4735b3835cc7a5499cc
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-06-09 19:15:22 -0400

    Rethinking specs

--- befunge.d
+++ befunge.d
@@ -1,26 +1,2 @@
 import std.stdio;
-class Stack
-{
-  this()
-  {
-    this.val=[];
-  }
-  private int[] val;
-  public int pop()
-  {
-    if(this.val.length==0)
-    {
-      return 0;
-    }
-    else
-    {
-      int tmp = val[val.length - 1];
-      this.val.length -= 1;
-      return tmp;
-    }
-  }
-  public void push(int arg)
-  {
-    this.val~=arg;
-  }
-}
+
