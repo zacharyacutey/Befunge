4532f88e08d4e89a810d75f6b56c5534120df5a4
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-06-15 16:27:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #Starting over, but in python, must install readchar for this to work!!!
+import readchar
+class Stack:
+  def __init__(self):
+    self.value = []
+  def pop(self):
+    if len(self.value) == 0:
+      return 0
+    return self.value.pop()
+  def push(self,value):
+    self.value.append(value)
