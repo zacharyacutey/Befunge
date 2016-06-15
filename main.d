1fdfe6c1938fe3ac0beeabf432dda3da412e8111
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-06-15 16:01:30 -0400

    Update main.d

--- main.d
+++ main.d
@@ -1,4 +1,14 @@
 import data.stack;
+int LEFT = 0;
+int RIGHT = 1;
+int UP = 2;
+int DOWN = 3;
 class Program : Stack!int
 {
+  public void Add()
+  {
+    int b = this.pop();
+    int a = this.pop();
+    this.push(a+b);
+  }
 }
