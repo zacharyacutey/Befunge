e876bbd10a4d7aa8f9504e5a7996636d60f2d04f
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-06-16 09:15:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import readchar
 
-def assert(n):
-  if not n:
-    throw Exception
+UNIT_TEST_MODE = True
+
+class Stack:
+  def __init__(self):
+    self.value = []
+  def pop(self):
+    if len(self.value) == 0:
+      return 0
+    else:
+      return self.value.pop()
+  def push(self,val):
+    self.value.append(val)
+#Unit test for stack
+if UNIT_TEST_MODE:
+  f = Stack()
+  assert(f.value == [])
+  f.push(2)
+  assert(f.value == [2])
+  f.push(3)
+  assert(f.value == [2,3])
+  assert(f.pop() == 3)
+  assert(f.value == [2])
+  assert(f.pop() == 2)
+  assert(f.value == [])
+  assert(f.pop() == 0)
+  
